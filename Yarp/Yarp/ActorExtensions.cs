37665895e8a9a8584737ec2f9da76b3f77b4d4a3
Philip Laureano <philip.laureano@gmail.com>
2018-03-13 12:01:38 +1100

    Added the ActorExtensions class

---
+++ Yarp/Yarp/ActorExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Yarp
+{
+    public static class ActorExtensions
+    {
+        public static Action<object> WithMessageHandler(this Func<Action<object>, Action<object>>
+            handlerFactory, Action<object> handler)
+        {
+            return handlerFactory(handler);
+        }
+
+        public static Func<Action<object>, Action<object>> CreateSender(this IActor actor,
+            CancellationToken token)
+        {
+            return handler => actor.CreateSender(handler, token);
+        }
+
+        public static Action<object> CreateSender(this IActor actor,
+            Action<object> outboundMessageHandler, CancellationToken token)
+        {
+            return async msg =>
+            {
+                var context = new Context(msg, outboundMessageHandler, token);
+                await actor.TellAsync(context);
+            };
+        }
+    }
+}
