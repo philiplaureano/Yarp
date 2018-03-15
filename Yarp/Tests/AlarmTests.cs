b34705f1123ccf8a870329f9e96c4ee8cc055e2f
Philip Laureano <philip.laureano@gmail.com>
2018-03-15 12:37:49 +1100

    Added alarm test stubs

---
+++ Yarp/Tests/AlarmTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class AlarmTests : IDisposable
+    {
+        [Fact]
+        public void ShouldBeAbleToSetAlarmForSpecificInterval()
+        {
+            throw new NotImplementedException("TODO: Implement ShouldBeAbleToSetAlarmForSpecificInterval");
+        }
+        [Fact]
+        public void ShouldBeAbleToCancelAlarm()
+        {
+            throw new NotImplementedException("TODO: Implement ShouldBeAbleToCancelAlarm");
+        }
+        [Fact]
+        public void ShouldBeAbleToSnoozeAlarm()
+        {
+            throw new NotImplementedException("TODO: Implement ShouldBeAbleToSnoozeAlarm");
+        }
+        
+        public void Dispose()
+        {
+        }
+    }
+}
