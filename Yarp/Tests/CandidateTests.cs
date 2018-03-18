7ce4ecdd9b0c9703f6c10283a29e066f8986ca86
Philip Laureano <philip.laureano@gmail.com>
2018-03-18 21:48:16 +1100

    Added stubs for the candidate tests

---
+++ Yarp/Tests/CandidateTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+using Yarp;
+
+namespace Tests
+{
+    public class CandidateTests
+    {
+        private RaftNode _raftNode;
+
+        public CandidateTests()
+        {
+        }
+
+        [Fact]
+        public void ShouldWinElectionIfMajorityOfVotesReceived()
+        {
+            throw new NotImplementedException("TODO: Implement ShouldWinElectionIfMajorityOfVotesReceived");
+        }
+
+        [Fact]
+        public void ShouldSendHeartbeatMessagesAfterWinningElection()
+        {
+            throw new NotImplementedException("TODO: Implement ShouldSendHeartbeatMessagesAfterWinningElection");
+        }
+    }
+}
